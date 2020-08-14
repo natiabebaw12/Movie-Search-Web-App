b28d360ee0f1e76c830c687b3b2f6859db235bbc
nati_abebaw <natiabebaw35@gmail.com>
2020-08-14 09:55:52 -0300

    Style added for App.js Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="container">
-          <img src="./movie_logo.png" className="logo"/>
-          <h1 className="title"> Movie search </h1>
-    </div>
-  );
+	return (
+		<div className="container">
+			<header className="header">
+				<img src="./movie_logo.png" className="logo" alt="Logo"/>
+				<h1 className="title"> Movie search </h1>
+			</header>
+		</div>
+	);
 }
 
 export default App;
